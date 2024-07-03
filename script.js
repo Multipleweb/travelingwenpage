2380d06dc59410bf6dcade39dcdd527da93fdc5f
siannun <nunsian2003@gmail.com>
2024-07-03 10:11:33 +0700

    generate qrcode

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-//validation form
-function validation () {
-    var username = document.getElementById('name').value;
-    var password = document.getElementById('pwd').value;
-    var cpassword = document.getElementById('cpwd').value;
+document.getElementById('generateBtn').addEventListener('click', function() {
+    var url = document.getElementById('urlInput').value;
+    if (url) {
+        // Validate URL
+        try {
+            new URL(url);
+        } catch (_) {
+            alert("Please enter a valid URL.");
+            return;
+        }
 
-    var predefinedUsername = "admin";
-    var predefinedPassword = "123";
+        var qrcodeContainer = document.getElementById('qrcode');
+        qrcodeContainer.innerHTML = '';
+        var qrcode = new QRCode(qrcodeContainer, {
+            text: url,
+            width: 230,
+            height: 230
+        });
 
-    if (username === "" || password === "") {
-        alert("Username and password cannot be empty!");
-        return false;
-    } else if (username !== predefinedUsername || password !== predefinedPassword) {
-        alert("Incorrect username or password!");
-        return false;
-    } else if(cpassword !== predefinedPassword) {
-        alert("Confirm Password please enter Password eqaul to Confirm Password");
-        return false;
-    }else{
-        alert("login success");
-        return true;
+        document.getElementById('downloadPngBtn').style.display = 'block';
+        document.getElementById('downloadJpgBtn').style.display = 'block';
+    }
+});
+
+document.getElementById('downloadPngBtn').addEventListener('click', function() {
+    downloadQRCode('image/png', 'qrcodegenerate.png');
+});
+
+document.getElementById('downloadJpgBtn').addEventListener('click', function() {
+    downloadQRCode('image/jpeg', 'qrcodegenerate.jpg');
+});
+
+function downloadQRCode(format, filename) {
+    var qrcodeCanvas = document.querySelector('.qrcode canvas');
+    if (qrcodeCanvas) {
+        var link = document.createElement('a');
+        link.href = qrcodeCanvas.toDataURL(format);
+        link.download = filename;
+        link.click();
     }
 }
